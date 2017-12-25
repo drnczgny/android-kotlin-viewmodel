7d097159f170dbaf54448dab1fa1a421f4fc019f
Adrian Czigany <c.adrian89@gmail.com>
2017-12-25 16:36:09 +0100

    init commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# android-kotlin-dagger2-starter
+
+Android kotlin application with dagger2 ContributesAndroidInjector. 
+- two activity and one fragmant build with ContributesAndroidInjector
+- model-s injected to the activity/fragment
+- fake api service is injected to the model-s
+
+ext {
+    supportLibraryVersion = '27.0.0'
+    constraintLayoutVersion = '1.0.2'
+    daggerVersion = '2.11'
+    rxVersion = '2.1.0'
+}
---
+++ app/src/main/java/com/adrian/project/di/AppComponent.kt
@@ -0,0 +1,26 @@
+package com.adrian.project.di
+
+import com.adrian.project.MyApplication
+import dagger.BindsInstance
+import dagger.Component
+import dagger.android.support.AndroidSupportInjectionModule
+import javax.inject.Singleton
+
+/**
+ * Created by cadri on 2017. 08. 03..
+ */
+
+@Singleton
+@Component(modules = arrayOf(AppModule::class, ActivityBuilderModule::class, AndroidSupportInjectionModule::class))
+interface AppComponent {
+
+    @Component.Builder
+    interface Builder {
+        @BindsInstance
+        fun application(myApplication: MyApplication): Builder
+
+        fun build(): AppComponent
+    }
+
+    fun inject(myApplication: MyApplication)
+}
